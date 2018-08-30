7eeded5061cac6de7ad531008119e756c898949c
David Navarro <davidnavarro@Davids-MacBook-Pro.local>
2018-08-29 18:03:59 -0700

    Adding contains and add methods to tree

--- JSEx04/script.js
+++ JSEx04/script.js
@@ -82,6 +82,29 @@ Tree.prototype.traverseBF = function (callback) {
     }
 };
 
+Tree.prototype.contains = function (callback, traversal) {
+    traversal.call(this, callback);
+};
+
+Tree.prototype.add = function (data, toData, traversal) {
+    var child = new Node(data),
+        parent = null,
+        callback = function (node) {
+            if (node.data === toData) {
+                parent = node;
+            }
+        };
+
+    this.contains(callback, traversal);
+
+    if (parent) {
+        parent.children.push(child);
+        child.parent = parent;
+    } else {
+        throw new Error('Cannot add node to a non-existent parent.');
+    }
+};
+
 //Example 2.1
 let tree = new Tree('one');
 
