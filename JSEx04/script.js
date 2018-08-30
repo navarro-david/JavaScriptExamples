8b133acfb414e99826dcfdc89c5166c8294e2f96
David Navarro <davidnavarro@Davids-MacBook-Pro.local>
2018-08-29 17:54:48 -0700

    Adding Depth and Breadth search in JSEx04

--- JSEx04/script.js
+++ JSEx04/script.js
@@ -1,3 +1,24 @@
+// Needed Data Sctructures
+
+/**
+* Queue
+*/
+function Queue() {
+    this.dataStore = []
+    this.enqueue = function enqueue(element) {
+        this.dataStore.push(element)
+    }
+    this.dequeue = function dequeue() {
+        return this.dataStore.shift()   // Returns the first element of the array
+    }
+    this.front = function front() {
+        return this.dataStore[0]
+    }
+    this.back = function back() {
+        return this.dataStore[this.dataStore.length - 1]
+    }
+}
+
 // Example 1: Classes in Vanilla JavaScript
 
 function Person(name, age) {
@@ -44,27 +65,66 @@ Tree.prototype.traverseDF = function (callback) {
     })(this._root);
 }
 
+Tree.prototype.traverseBF = function (callback) {
+    var queue = new Queue();
+
+    queue.enqueue(this._root);
+
+    currentTree = queue.dequeue();
+
+    while (currentTree) {
+        for (var i = 0, length = currentTree.children.length; i < length; i++) {
+            queue.enqueue(currentTree.children[i]);
+        }
+
+        callback(currentTree);
+        currentTree = queue.dequeue();
+    }
+};
+
 //Example 2.1
 let tree = new Tree('one');
- 
+
 tree._root.children.push(new Node('two'));
 tree._root.children[0].parent = tree;
- 
+
 tree._root.children.push(new Node('three'));
 tree._root.children[1].parent = tree;
- 
+
 tree._root.children.push(new Node('four'));
 tree._root.children[2].parent = tree;
- 
+
 tree._root.children[0].children.push(new Node('five'));
 tree._root.children[0].children[0].parent = tree._root.children[0];
- 
+
 tree._root.children[0].children.push(new Node('six'));
 tree._root.children[0].children[1].parent = tree._root.children[0];
- 
+
 tree._root.children[2].children.push(new Node('seven'));
 tree._root.children[2].children[0].parent = tree._root.children[2];
 
-tree.traverseDF(function(node) {
+/*
+ 
+ structure of tree
+ 
+ one (depth: 0)
+ ├── two (depth: 1)
+ │   ├── five (depth: 2)
+ │   └── six (depth: 2)
+ ├── three (depth: 1)
+ └── four (depth: 1)
+     └── seven (depth: 2)
+ 
+ */
+
+console.log("DEPTH-FIRST SEARCH");
+
+tree.traverseDF(function (node) {
+    console.log(node.data)
+});
+
+console.log("BREADTH-FIRST SEARCH");
+
+tree.traverseBF(function (node) {
     console.log(node.data)
 });
